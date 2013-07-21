882bc0cff498594b51d09a9a3dd2a1c39d5aedc3
Guillaume <g@guillaumeduval.fr>
2013-07-21 14:46:57 +0200

    màj

--- py/nox_sms_backup/backup.py
+++ py/nox_sms_backup/backup.py
@@ -4,11 +4,14 @@ import re
 import os
 import json
 
+from codecs import open
 from datetime import datetime
 from hashlib import md5
 
 DEFAULT_REP = "messages"
 DEFAULT_BACKUP = "messages/sms_backup.json"
+
+all_sms = ""
 
 class SMS:
     """
@@ -20,7 +23,7 @@ class SMS:
 
     regexp = re.compile(r"(?P<texte>.+),'(?P<nom>[^,]*),(?P<date>[^,]*),$")
 
-    def __init__(self, date=0, texte="", expediteur="", destinataire="", parse=None, not_sent=None):
+    def __init__(self, date=0, texte="", expediteur=None, destinataire=None, parse=None, not_sent=None):
         """
             Le constructeur permet de remplir les champs séparément ou de parser une source.
             Il crée ensuite une somme md5 du message pour fournir un identifiant unique.
@@ -39,6 +42,15 @@ class SMS:
             self.id = self.id.hexdigest()
         else:
             self.id = None
+
+    def jsonable(self):
+        return {
+            "id": self.id,
+            "date": self.date.strftime("%Y-%m-%d_%H:%M:%S"),
+            "texte": self.texte,
+            "expediteur": self.expediteur,
+            "destinataire": self.destinataire,
+        }
 
     def __repr__(self):
         return "<SMS {}{} le {}>".format(self.expediteur or "@", self.destinataire or "@", self.date)
@@ -97,22 +109,28 @@ def main():
     lignes = []
     ligne_en_cours = ""
     for fichier, not_sent in fichiers.items():
-        with open("{}/{}".format(DEFAULT_REP, fichier)) as file: #FIXME : codecs
+        with open("{}/{}".format(DEFAULT_REP, fichier), encoding="utf-16-le") as file: #FIXME : codecs
             for line in file:
-                ligne_en_cours += line.replace("\x00", "") + "\n" #ajoute le texte nettoyé
+                ligne_en_cours += line + "\n" #ajoute le texte nettoyé
 
-                if ",\x00'" in line:
+                if ",'" in line:
                     lignes.append(ligne_en_cours[:-1]) # sans le \n restant
                     ligne_en_cours=""
         fichiers[fichier] = (not_sent, lignes)
 
-
+    # on charge la sauvegarde si elle existe
     if os.path.exists(DEFAULT_BACKUP):
         with open(DEFAULT_BACKUP) as file:
-            all_sms = json.load(DEFAULT_BACKUP)
+            try:
+                all_sms = json.load(file)
+                json_charge = True
+            except ValueError:
+                json_charge=False;all_sms = {} #raise Exception("Le fichier JSON n'est pas valide.")
     else:
         all_sms = {}
+        json_charge = False
     
+    # enregistre les SMS
     for fichier, infos in fichiers.items():
         for sms_brut in infos[1]:
             sms=SMS(parse=sms_brut, not_sent=infos[0])
@@ -120,8 +138,22 @@ def main():
             if sms.id is not None and sms.id not in all_sms.keys():
                 all_sms[sms.id] = sms
 
+    # on sauvegarde et on backup si nécessaire
+    if json_charge:
+        with open(DEFAULT_BACKUP) as original_file:
+            with open(
+                    "{}.{}.bak".format(
+                        DEFAULT_BACKUP, 
+                        datetime.now().strftime("%Y-%m-%d_%Hh%Mmin%Ss")
+                    )
+                ) as backup_file:
+                for line in original_file:
+                    backup_file.write(line)
+
     with open(DEFAULT_BACKUP, "wb") as file:
-        json.dump(all_sms, file)
+        for i, sms in enumerate(all_sms.values()):
+            printi)
+            json.dump(sms.jsonable(), file)
 
 if __name__ == '__main__':
     main()
